42fba415d3a3aec52368cb213a7b786d81ad6bde
Nannings <mrnannings@gmail.com>
2019-01-22 12:03:19 +0100

    008

---
+++ Assets/Scripts/Weapons/HandgunFire.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandgunFire : MonoBehaviour
+{
+    public GameObject theGun;
+    public GameObject muzzleFlash;
+    public AudioSource gunFire;
+    public bool isFiring = false;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!isFiring)
+            {
+                StartCoroutine(FiringHandgun());
+            }
+        }
+    }
+
+    IEnumerator FiringHandgun()
+    {
+        isFiring = true;
+        theGun.GetComponent<Animator>().Play("HandgunFire");
+        muzzleFlash.SetActive(true);
+        gunFire.Play();
+        yield return new WaitForSeconds(.05f);
+        muzzleFlash.SetActive(false);
+        yield return new WaitForSeconds(.25f);
+        isFiring = false;
+        theGun.GetComponent<Animator>().Play("New State");
+    }
+}
